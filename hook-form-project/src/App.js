3d417d210ee33f674e94ef29e2f0cbf8df484b5b
Theodore Cady <72614259+TedCady@users.noreply.github.com>
2021-01-08 08:58:10 -0500

    Add files via upload

---
+++ hook-form-project/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, { useState }  from "react";
+import Form from "./components/Form";
+import Results from "./components/Results";
+
+function App() {
+const [state, setState] = useState({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+});
+  return (
+    <div className="App">
+    <Form inputs={state} formInputs={setState}/>
+    <Results info={state}/>
+    </div>
+  );
+}
+
+export default App;
